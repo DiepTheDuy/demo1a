16aa58579d055c42391c9ac52512153f0916f93c
Nguyễn Thanh Tú <ntt11052000@gmail.com>
2021-01-06 12:56:56 +0700

    Mot phan

--- Lab05-Report/Subforms/Product.Designer.cs
+++ Lab05-Report/Subforms/Product.Designer.cs
@@ -29,19 +29,266 @@ namespace Lab05_Report.Subforms
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbDVT = new System.Windows.Forms.ComboBox();
+            this.txtGiaBan = new System.Windows.Forms.TextBox();
+            this.txtGiaMua = new System.Windows.Forms.TextBox();
+            this.txtTenSP = new System.Windows.Forms.TextBox();
+            this.txtMaSP = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dgvThongTinSP = new System.Windows.Forms.DataGridView();
+            this.Stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaSP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.donvi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.giaMua = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.giaBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThongTinSP)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(271, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thong tin san pham";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ma san pham";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ten san pham";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Don vi tinh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Gia mua";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gia ban";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbDVT);
+            this.groupBox1.Controls.Add(this.txtGiaBan);
+            this.groupBox1.Controls.Add(this.txtGiaMua);
+            this.groupBox1.Controls.Add(this.txtTenSP);
+            this.groupBox1.Controls.Add(this.txtMaSP);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(23, 92);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(263, 226);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thong tin san pham";
+            // 
+            // cbDVT
+            // 
+            this.cbDVT.FormattingEnabled = true;
+            this.cbDVT.Location = new System.Drawing.Point(121, 105);
+            this.cbDVT.Name = "cbDVT";
+            this.cbDVT.Size = new System.Drawing.Size(121, 21);
+            this.cbDVT.TabIndex = 10;
+            // 
+            // txtGiaBan
+            // 
+            this.txtGiaBan.Location = new System.Drawing.Point(121, 169);
+            this.txtGiaBan.Name = "txtGiaBan";
+            this.txtGiaBan.Size = new System.Drawing.Size(100, 20);
+            this.txtGiaBan.TabIndex = 9;
+            // 
+            // txtGiaMua
+            // 
+            this.txtGiaMua.Location = new System.Drawing.Point(121, 136);
+            this.txtGiaMua.Name = "txtGiaMua";
+            this.txtGiaMua.Size = new System.Drawing.Size(100, 20);
+            this.txtGiaMua.TabIndex = 8;
+            // 
+            // txtTenSP
+            // 
+            this.txtTenSP.Location = new System.Drawing.Point(121, 74);
+            this.txtTenSP.Name = "txtTenSP";
+            this.txtTenSP.Size = new System.Drawing.Size(100, 20);
+            this.txtTenSP.TabIndex = 7;
+            // 
+            // txtMaSP
+            // 
+            this.txtMaSP.Location = new System.Drawing.Point(121, 41);
+            this.txtMaSP.Name = "txtMaSP";
+            this.txtMaSP.Size = new System.Drawing.Size(100, 20);
+            this.txtMaSP.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnBack);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Location = new System.Drawing.Point(23, 380);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(276, 53);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chuc nang";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(188, 19);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 8;
+            this.btnBack.Text = "Tro lai";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(96, 19);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Xoa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(6, 19);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Cap nhat";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // dgvThongTinSP
+            // 
+            this.dgvThongTinSP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThongTinSP.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Stt,
+            this.MaSP,
+            this.donvi,
+            this.giaMua,
+            this.giaBan});
+            this.dgvThongTinSP.Location = new System.Drawing.Point(294, 92);
+            this.dgvThongTinSP.Name = "dgvThongTinSP";
+            this.dgvThongTinSP.Size = new System.Drawing.Size(544, 282);
+            this.dgvThongTinSP.TabIndex = 8;
+            this.dgvThongTinSP.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThongTinSP_CellContentClick);
+            // 
+            // Stt
+            // 
+            this.Stt.HeaderText = "STT";
+            this.Stt.Name = "Stt";
+            // 
+            // MaSP
+            // 
+            this.MaSP.HeaderText = "Ma san pham";
+            this.MaSP.Name = "MaSP";
+            // 
+            // donvi
+            // 
+            this.donvi.HeaderText = "DVT";
+            this.donvi.Name = "donvi";
+            // 
+            // giaMua
+            // 
+            this.giaMua.HeaderText = "Gia mua";
+            this.giaMua.Name = "giaMua";
+            // 
+            // giaBan
+            // 
+            this.giaBan.HeaderText = "Gia ban";
+            this.giaBan.Name = "giaBan";
             // 
             // frmProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(873, 450);
+            this.Controls.Add(this.dgvThongTinSP);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
             this.Name = "frmProduct";
             this.Text = "Product";
+            this.Load += new System.EventHandler(this.frmProduct_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThongTinSP)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbDVT;
+        private System.Windows.Forms.TextBox txtGiaBan;
+        private System.Windows.Forms.TextBox txtGiaMua;
+        private System.Windows.Forms.TextBox txtTenSP;
+        private System.Windows.Forms.TextBox txtMaSP;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView dgvThongTinSP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Stt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaSP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn donvi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn giaMua;
+        private System.Windows.Forms.DataGridViewTextBoxColumn giaBan;
     }
 }
--- Lab05-Report/Subforms/Product.cs
+++ Lab05-Report/Subforms/Product.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Lab05_Report.Models;
 
 namespace Lab05_Report.Subforms
 {
     public partial class frmProduct : Form
     {
+        List<Product> list;
+        ProductContextDB context = new ProductContextDB();
         public frmProduct()
         {
             InitializeComponent();
+        }
+        private void LoadGrid()
+        {
+            list = context.Products.ToList();
+            dgvThongTinSP.DataSource = list;
+        }
+
+        private void frmProduct_Load(object sender, EventArgs e)
+        {
+            LoadGrid();
+
+            List<Product> listpr = context.Products.ToList();
+            cbDVT.DataSource = listpr;
+            cbDVT.ValueMember = "";
+            cbDVT.DisplayMember = "";
+        }
+        private void ResetValue()
+        {
+            txtMaSP.Text = "";
+            txtTenSP.Text = "";
+            txtGiaBan.Text = "";
+            txtGiaMua.Text = "";
+        }
+
+        private void dgvThongTinSP_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                DataGridViewRow dgvRow = dgvThongTinSP.Rows[e.RowIndex];
+                txtMaSP.Text = dgvRow.Cells[0].Value.ToString();
+                txtTenSP.Text = dgvRow.Cells[1].Value.ToString();                
+                cbDVT.SelectedValue = dgvRow.Cells[2].Value;
+                txtGiaBan.Text = dgvRow.Cells[3].Value.ToString();
+                txtGiaMua.Text = dgvRow.Cells[4].Value.ToString();
+            }
         }
     }
 }
